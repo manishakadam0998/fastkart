8bbf8397d8ee55f1419745130b286d6252491ae1
Manisha <manishakadam0998@gmail.com>
2023-09-05 11:45:59 +0530

    fastkart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './component/home/home.component';
+import { ProductComponent } from './component/product/product.component';
+import { ProductListComponent } from './component/product-list/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProductComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule,
